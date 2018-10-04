c0800017e61e2cd30b36bd4b9ba6aec164d821ab
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-10-04 10:36:45 +0200

    ajouté le téléchargement de docs dans Soutenir

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,18 @@
 class PagesController < ApplicationController
   layout "home"
-
   # skip_before_action :authenticate_user!, only: [:home]
+
+  def dossier_de_mecenat
+    send_file "#{Rails.root}/app/assets/documents/dossier_de_mecenat.pdf", type: "application/pdf", x_sendfile: true
+  end
+
+  def charte_graphique
+    send_file "#{Rails.root}/app/assets/documents/charte_graphique.pdf", type: "application/pdf", x_sendfile: true
+  end
+
+  def logo_RVB
+    send_file "#{Rails.root}/app/assets/documents/logo_RVB.svg", type: "application/svg", x_sendfile: true
+  end
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/inscription' => 'pages#inscription'
   get '/soutenir' => 'pages#soutenir'
   get '/partenaires' => 'pages#partenaires'
+  get 'dossier_de_mecenat', to: "pages#dossier_de_mecenat"
+  get 'charte_graphique', to: "pages#charte_graphique"
+  get 'logo_RVB', to: "pages#logo_RVB"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
