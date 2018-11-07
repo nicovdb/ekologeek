c664ec0bbc8c712b9f9a6536391cff11ab04f59b
charles-henri poniard <c.poniard@gmail.com>
2018-11-07 19:50:03 +0100

    remove link in mobile on navbar

--- app/javascript/components/data-toggle.js
+++ app/javascript/components/data-toggle.js
@@ -1,13 +1,14 @@
-$(window).on('resize', function() {
-    if($(window).width() < 992) {
-      var elements = document.querySelectorAll("#navbarDropdownMenuLink");
-      elements.forEach((element)=> {
-        element.setAttribute("data-toggle", "dropdown");
-      });
-    }else {
-      var elements = document.querySelectorAll("#navbarDropdownMenuLink");
-      elements.forEach((element)=> {
-        element.removeAttribute("data-toggle", "dropdown");
-      });
-    }
-})
+// $(window).on('resize', function() {
+//   if($(window).width() < 992) {
+//     let elements = document.querySelectorAll("#navbarDropdownMenuLink");
+//     console.log(element);
+//     elements.forEach((element)=> {
+//       element.setAttribute("data-toggle", "dropdown");
+//     });
+//   } else {
+//     let elements = document.querySelectorAll("#navbarDropdownMenuLink");
+//     elements.forEach((element)=> {
+//       element.removeAttribute("data-toggle", "dropdown");
+//     });
+//   }
+// })
