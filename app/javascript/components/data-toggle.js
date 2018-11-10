f214f1b1428cd79ad5bb571a45d68b940c03ae9d
charles-henri poniard <c.poniard@gmail.com>
2018-11-10 14:50:01 +0100

    remove js file data-toggle

--- app/javascript/components/data-toggle.js
+++ app/javascript/components/data-toggle.js
@@ -1,14 +1,1 @@
-// $(window).on('resize', function() {
-//   if($(window).width() < 992) {
-//     let elements = document.querySelectorAll("#navbarDropdownMenuLink");
-//     console.log(element);
-//     elements.forEach((element)=> {
-//       element.setAttribute("data-toggle", "dropdown");
-//     });
-//   } else {
-//     let elements = document.querySelectorAll("#navbarDropdownMenuLink");
-//     elements.forEach((element)=> {
-//       element.removeAttribute("data-toggle", "dropdown");
-//     });
-//   }
-// })
+
