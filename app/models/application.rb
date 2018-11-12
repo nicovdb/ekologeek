3d8eba6fed10cbe5dfa38c603eefb418e0c74fd5
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-11-12 15:14:50 +0100

    added associations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,11 @@
 class Application < ApplicationRecord
   belongs_to :company
+  has_many :application_company_know_hows
+  has_many :company_know_hows, through: :application_company_know_hows
+  has_many :application_trash_providers
+  has_many :trash_providers, through: :application_trash_providers
+  has_many :application_trash_working_types
+  has_many :trash_working_types, through: :application_trash_working_types
+  has_many :application_dispositives
+  has_many :dispositives, through: :application_dispositives
 end
