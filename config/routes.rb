4dd034bc740e4aa15bcea4d45242d79c9c3e0965
charles-henri poniard <c.poniard@gmail.com>
2018-11-13 22:35:36 +0100

    bug with password devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   root to: 'pages#home'
   get '/pourquoi' => 'pages#pourquoi'
   get '/comment' => 'pages#comment'
