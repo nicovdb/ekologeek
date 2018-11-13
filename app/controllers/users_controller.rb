a286b63edf19ea83afe82d5cfc8392a6d52f4a25
charles-henri poniard <c.poniard@gmail.com>
2018-11-13 16:37:27 +0100

    add style to users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
 
   layout "connected"
 
   def show
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
