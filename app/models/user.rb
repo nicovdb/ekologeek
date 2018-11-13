765577ed0aa461c0acac7fb0e214f7e56a4d9799
charles-henri poniard <c.poniard@gmail.com>
2018-11-13 11:07:14 +0100

    remove has_many referents in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :referent
   belongs_to :company
 
 
