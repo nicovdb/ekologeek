968ff9bd3b68bc83905c10d35e42e8c5625432c4
charles-henri poniard <c.poniard@gmail.com>
2018-11-15 20:32:46 +0100

    add modif in partie 2 google drive

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,9 @@ class PagesController < ApplicationController
   def charte_graphique
     send_file "#{Rails.root}/app/assets/documents/charte_graphique.pdf", type: "application/pdf", x_sendfile: true
   end
+  def communique_de_press
+    send_file "#{Rails.root}/app/assets/documents/communique_de_press.pdf", type: "application/pdf", x_sendfile: true
+  end
 
   def logo_RVB
     send_file "#{Rails.root}/app/assets/documents/logo_RVB.svg", type: "application/svg", x_sendfile: true
