bb40a0133278b87a92e83df3294d18527bbb40bd
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-11-16 12:18:54 +0100

    bundle update

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,4 +1,5 @@
 class Form < ApplicationRecord
+
   belongs_to :company
   has_many :form_company_know_hows
   has_many :company_know_hows, through: :form_company_know_hows
@@ -8,4 +9,5 @@ class Form < ApplicationRecord
   has_many :trash_working_types, through: :form_trash_working_types
   has_many :form_dispositives
   has_many :dispositives, through: :form_dispositives
+
 end
