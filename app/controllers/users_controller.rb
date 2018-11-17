e9f6e2cdeb9a96594259f0d8948fa35b8e7cc467
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-11-17 18:04:04 +0100

    redirection si mauvais update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,11 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def unknown
