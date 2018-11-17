d99aab2102e018dd170b69f25051beffa9f48097
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-11-17 18:07:13 +0100

    reglé le problème du update user en rajoutant un authenticate obligatoire

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :unknown]
+  before_action :authenticate_user!, only: [:show, :edit, :update]
 
   layout "connected"
 
