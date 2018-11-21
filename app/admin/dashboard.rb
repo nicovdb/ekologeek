e39926387bc71913c024ae4f4c4fdf529e730f40
charles-henri poniard <c.poniard@gmail.com>
2018-11-21 11:26:26 +0100

    add link to admin nav and footer, removes comments

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register_page "Dashboard" do
 
+
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
@@ -10,24 +11,21 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
+
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
     #   column do
-    #     panel "Recent Posts" do
+    #     panel "Liens" do
     #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
+    #         li link_to(
+    #           label: "heroku",
+    #           url: "https://id.heroku.com/login",
+    #           html_options: { target: :blank }
+    #         )
     #       end
     #     end
     #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
     # end
-  end # content
+  end
 end
