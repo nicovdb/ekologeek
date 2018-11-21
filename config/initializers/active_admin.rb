7fafe1cd7cac8870232500dfae9243899f2d21aa
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-11-21 11:55:36 +0100

    root to Forms#index

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -9,7 +9,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Ekologeek"
+  config.site_title = "Défi Zéro Déchet Pro"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -117,7 +117,7 @@ ActiveAdmin.setup do |config|
   # roots for each namespace.
   #
   # Default:
-  # config.root_to = 'dashboard#index'
+  config.root_to = 'forms#index'
 
   # == Admin Comments
   #
