23e0d4f82bf09ab90e4c1345b9022237335abddd
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-12-30 12:57:17 +0100

    graph dynamique fonctionne yipee

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,6 +3,20 @@ class ChartsController < ApplicationController
   layout "charts"
 
   def index
+    sql = "SELECT start_at, end_at, weight_person_day, bin_types.name FROM collects
+                JOIN bins ON bins.id = collects.bin_id
+                JOIN companies ON companies.id = bins.company_id
+                JOIN bin_types ON bin_types.id = bins.bin_type_id
+                WHERE companies.id = #{current_user.company.id}"
+    @collects = ActiveRecord::Base.connection.execute(sql)
+    @types_collects = @collects.group_by { |collect| collect["name"]}
+    @series = @types_collects.map { |type_collects|
+      @data = type_collects[1].map { |type_collect| {x: (type_collect["start_at"].to_time.to_i * 1000), x2: (type_collect["end_at"].to_time.to_i * 1000), y: type_collect["weight_person_day"]}}
+      {
+        name: type_collects.first,
+        data: @data
+      }
+    }
   end
 
   def kilo_per_employee_per_day
