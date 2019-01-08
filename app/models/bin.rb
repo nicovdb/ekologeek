f46d8aad521deae4e2bd6df70776669b371428b2
charles-henri poniard <c.poniard@gmail.com>
2019-01-08 12:22:44 +0100

    validation for collect, bin and trashDiagnistic / change edit to bins

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -4,4 +4,6 @@ class Bin < ApplicationRecord
   has_many :collects, dependent: :destroy
   has_many :trash_bins, dependent: :destroy
   has_many :trashes, through: :trash_bins
+
+  validates :volume,:bin_type_id, :cost, :frequency_number, :frequency_periodicity, :collector, presence: true
 end
--- app/models/trash_diagnostic.rb
+++ app/models/trash_diagnostic.rb
@@ -1,3 +1,5 @@
 class TrashDiagnostic < ApplicationRecord
   belongs_to :company
+
+  validates :annual_cost, :reduction_objective, :employees_nb, presence: true
 end
