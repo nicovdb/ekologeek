c03158e17ad48eab48f324a914505d07be5f9d3e
charles-henri poniard <c.poniard@gmail.com>
2019-01-11 15:00:47 +0100

    change validations questions company_behaviour, only integer Q1, add step_4

--- app/models/company_behaviour.rb
+++ app/models/company_behaviour.rb
@@ -5,12 +5,10 @@ class CompanyBehaviour < ApplicationRecord
 
   has_many :result_actions, dependent: :destroy
 
-  validates :participants_nb, presence: true, if: :step_one
-  validates :employees_sensitized,
-            :actions_displayed,
+  validates :participants_nb, presence: true, if: :step_one?
+  validates :actions_displayed,
             :strategic_display,
             :newbies_sensitized,
             :indicators_communicated,
-            presence: true, if: :step_two
-  validates :daily_actions_diag, presence: true, if: :step_three
+            presence: true, if: :step_two?
 end
