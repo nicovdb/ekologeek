00beeab3cd46f3e56ef313d6559219707b304e6b
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2019-01-14 11:40:39 +0100

    meilleur format d'affichage

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,12 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+import { French } from "flatpickr/dist/l10n/fr.js"
 
 flatpickr("#range_start", {
   altInput: true,
-  plugins: [new rangePlugin({ input: "#range_end"})]
-})
+  plugins: [new rangePlugin({ input: "#range_end"})],
+  "locale": French,
+  altFormat: "l d F Y"
+});
+
