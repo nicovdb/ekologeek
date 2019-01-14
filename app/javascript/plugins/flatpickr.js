19bbbe11b37e38d0f43dc39676c547ec7f1ca94c
charles-henri poniard <c.poniard@gmail.com>
2019-01-14 12:11:50 +0100

    remove range, add validation option, add display validation to bin_id in collects#new

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,12 +1,10 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 import { French } from "flatpickr/dist/l10n/fr.js"
 
-flatpickr("#range_start", {
+flatpickr(".datepicker", {
   altInput: true,
-  plugins: [new rangePlugin({ input: "#range_end"})],
   "locale": French,
-  altFormat: "l d F Y"
+  altFormat: "l d F Y",
+  maxDate: new Date()
 });
-
