99dbb97d3b7201f9f47400c5efca5e5dfb3d8a0f
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2019-01-14 11:18:17 +0100

    mise en place du datepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import '../components/scroll.js';
 import '../components/anchor.js';
 import '../components/add-bin.js';
 import '../components/dropable.js';
+import "../plugins/flatpickr.js";
 
 import Highcharts from "highcharts/highcharts.js";
 import * as HighchartsMore from "highcharts/highcharts-more.js";
